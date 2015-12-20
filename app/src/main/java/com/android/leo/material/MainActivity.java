6c84a8d555e4441a596e3e7f435e78c212c5e238
leo <leohu1105@icloud.com>
2015-12-20 15:35:25 +0800

    add NavigationView

--- app/src/main/java/com/android/leo/material/MainActivity.java
+++ app/src/main/java/com/android/leo/material/MainActivity.java
@@ -2,11 +2,20 @@ package com.android.leo.material;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private NavigationView nav;
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +27,66 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                Snackbar.make(v, "Fab", Snackbar.LENGTH_SHORT).setAction("cancel", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "click snackbar cancel", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
             }
         });
+        nav = (NavigationView) findViewById(R.id.nav);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawyer);
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_item_1:
+                        Toast.makeText(MainActivity.this, "click item1", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.navigation_item_2:
+                        Toast.makeText(MainActivity.this, "click item2", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+
+        //join drawyerlayout and toolbar
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+
+
     }
 
     @Override
